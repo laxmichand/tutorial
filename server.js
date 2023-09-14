8b05d77da977b3cf3feff69d883b4f403b43e17d
Laxmichandra Dhuvare <l.dhuvare@zensar.com>
2023-09-15 00:35:02 +0530

    helmet added

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const path = require('path');
 var cors = require('cors');
+const helmet = require('helmet'); // Import the helmet middleware
+
 
 const app = express();
 
 app.use((express.json({ limit: "30mb", extended: true})))
 app.use((express.urlencoded({ limit: "30mb", extended: true})))
 app.use((cors()));
+app.use(helmet()); // Use the helmet middleware to enhance security
 
 const tutRouter = require('./backend/src/routes/tutorials.route');
 app.use("/tutorials", tutRouter);
